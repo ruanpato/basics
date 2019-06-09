a76db56aafa18772b7b5b5fbe4af7ee5c8507113
ruanpato <ruanpato@outlook.com>
2019-06-08 23:11:13 -0300

    misc dir created to miscellaneous

---
+++ pyThings/misc/tropas.py
@@ -0,0 +1,7 @@
+class Tropa:
+    def __init__(self, quantidade, tipo, poder):
+        self.quantidade = quantidade
+        self.tipo = tipo
+        self.poder = poder
+    
+qtd = input("Quantidade de comandantes: ")
